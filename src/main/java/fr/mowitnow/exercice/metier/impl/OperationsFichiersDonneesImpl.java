9de8e299cc5c16fe49404ba6e6619a2632f07525
unknown <k-michael@hotmail.fr>
2020-07-26 20:22:37 +0200

    Vérification chemin null

--- src/main/java/fr/mowitnow/exercice/metier/impl/OperationsFichiersDonneesImpl.java
+++ src/main/java/fr/mowitnow/exercice/metier/impl/OperationsFichiersDonneesImpl.java
@@ -32,38 +32,40 @@ public class OperationsFichiersDonneesImpl implements OperationsFichiersDonnees 
 		
 		InformationsEntree informationsEntree = null;
 		
-		try {
-			BufferedReader reader = Files.newBufferedReader(Paths.get(chemin));
-			List<String> lignesFichier = reader.lines().collect(Collectors.toList());
-			
-			informationsEntree = new InformationsEntree();
-			
-			String ligneGrille = lignesFichier.get(0);
-			String[] infosLigneGrille = ligneGrille.split(CARACTERE_ESPACE);
-			int maxX = Integer.parseInt(infosLigneGrille[0]);
-			int maxY = Integer.parseInt(infosLigneGrille[1]);
-			Grille grille = new Grille(maxX, maxY);
-			
-			for (int i = 1; i < lignesFichier.size(); i += 2) {
-				Tondeuse tondeuse = new Tondeuse();
-				String lignePositionTondeuse = lignesFichier.get(i);
-				String[] infosLignePositionTondeuse = lignePositionTondeuse.split(CARACTERE_ESPACE);
-				int positionX = Integer.parseInt(infosLignePositionTondeuse[0]);
-				int positionY = Integer.parseInt(infosLignePositionTondeuse[1]);
-				PointCardinal pointCardinal = PointCardinal.valueOf(infosLignePositionTondeuse[2]);
-				String instructionTondeuse = lignesFichier.get(i + 1);
-				tondeuse.setOrientation(pointCardinal);
-				tondeuse.setPositionX(positionX);
-				tondeuse.setPositionY(positionY);
-				tondeuse.setInstruction(instructionTondeuse);
-				informationsEntree.ajouterTondeuse(tondeuse);
+		if (chemin != null) {
+			try {
+				BufferedReader reader = Files.newBufferedReader(Paths.get(chemin));
+				List<String> lignesFichier = reader.lines().collect(Collectors.toList());
+				
+				informationsEntree = new InformationsEntree();
+				
+				String ligneGrille = lignesFichier.get(0);
+				String[] infosLigneGrille = ligneGrille.split(CARACTERE_ESPACE);
+				int maxX = Integer.parseInt(infosLigneGrille[0]);
+				int maxY = Integer.parseInt(infosLigneGrille[1]);
+				Grille grille = new Grille(maxX, maxY);
+				
+				for (int i = 1; i < lignesFichier.size(); i += 2) {
+					Tondeuse tondeuse = new Tondeuse();
+					String lignePositionTondeuse = lignesFichier.get(i);
+					String[] infosLignePositionTondeuse = lignePositionTondeuse.split(CARACTERE_ESPACE);
+					int positionX = Integer.parseInt(infosLignePositionTondeuse[0]);
+					int positionY = Integer.parseInt(infosLignePositionTondeuse[1]);
+					PointCardinal pointCardinal = PointCardinal.valueOf(infosLignePositionTondeuse[2]);
+					String instructionTondeuse = lignesFichier.get(i + 1);
+					tondeuse.setOrientation(pointCardinal);
+					tondeuse.setPositionX(positionX);
+					tondeuse.setPositionY(positionY);
+					tondeuse.setInstruction(instructionTondeuse);
+					informationsEntree.ajouterTondeuse(tondeuse);
+				}
+				
+				informationsEntree.setGrille(grille);
+				
+			} catch (IOException e) {
+				informationsEntree = null;
+				e.printStackTrace();
 			}
-			
-			informationsEntree.setGrille(grille);
-			
-		} catch (IOException e) {
-			informationsEntree = null;
-			e.printStackTrace();
 		}
 		
 		return informationsEntree;
