d13488397f15b9678ea53c9aba6ca1ffc6e49ee3
unknown <k-michael@hotmail.fr>
2020-07-26 17:10:24 +0200

    Initialisation de la classe principale

---
+++ src/main/java/fr/mowitnow/exercice/Application.java
@@ -0,0 +1,13 @@
+package fr.mowitnow.exercice;
+
+/**
+ * Classe principale de l'application
+ */
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
