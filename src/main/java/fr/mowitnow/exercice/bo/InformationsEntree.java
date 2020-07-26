4b9313a3f1dc3b47ce90ab03f3d83809f53866d9
unknown <k-michael@hotmail.fr>
2020-07-26 18:07:17 +0200

    Création de la classe InformationsEntree

---
+++ src/main/java/fr/mowitnow/exercice/bo/InformationsEntree.java
@@ -0,0 +1,54 @@
+package fr.mowitnow.exercice.bo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Représentation des informations reçues en entrée
+ */
+public class InformationsEntree {
+	
+	/**
+	 * Grille de départ
+	 */
+	private Grille grille;
+	
+	/**
+	 * Liste de tondeuses
+	 */
+	private List<Tondeuse> listeTondeuses;
+	
+	/**
+	 * Constructeur par défaut
+	 */
+	public InformationsEntree() {
+		this.listeTondeuses = new ArrayList<Tondeuse>();
+	}
+
+	/**
+	 * Getter grille
+	 * 
+	 * @return the grille
+	 */
+	public Grille getGrille() {
+		return grille;
+	}
+
+	/**
+	 * Setter grille
+	 * 
+	 * @param grille the grille to set
+	 */
+	public void setGrille(Grille grille) {
+		this.grille = grille;
+	}
+	
+	/**
+	 * Ajouter une tondeuse dans la liste
+	 * 
+	 * @param tondeuse
+	 */
+	public void ajouterTondeuse(Tondeuse tondeuse) {
+		this.listeTondeuses.add(tondeuse);
+	}
+}
