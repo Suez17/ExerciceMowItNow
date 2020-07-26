2cc94a2b2197a3163e0bdd6df98b322ecfa9ea28
unknown <k-michael@hotmail.fr>
2020-07-26 23:57:11 +0200

    Implémentation main

--- src/main/java/fr/mowitnow/exercice/Application.java
+++ src/main/java/fr/mowitnow/exercice/Application.java
@@ -1,13 +1,24 @@
 package fr.mowitnow.exercice;
 
+import java.io.IOException;
+
+import fr.mowitnow.exercice.bo.InformationsEntree;
+import fr.mowitnow.exercice.metier.InterpreteurParcoursTondeuses;
+import fr.mowitnow.exercice.metier.OperationsFichiersDonnees;
+import fr.mowitnow.exercice.metier.impl.InterpreteurParcoursTondeusesImpl;
+import fr.mowitnow.exercice.metier.impl.OperationsFichiersDonneesImpl;
+
 /**
  * Classe principale de l'application
  */
 public class Application {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		String cheminFichierEntree = args[0];
+		InterpreteurParcoursTondeuses interpreteurParcoursTondeuses = new InterpreteurParcoursTondeusesImpl();
+		OperationsFichiersDonnees operationsFichiersDonnees = new OperationsFichiersDonneesImpl();
+		InformationsEntree informationsEntree = operationsFichiersDonnees.lireFichierEntree(cheminFichierEntree);
+		interpreteurParcoursTondeuses.interpreterParcoursTondeuses(informationsEntree);
 	}
 
 }
