419d0fe20f3b7a9b016222762a405f83aa95e1a0
unknown <k-michael@hotmail.fr>
2020-07-26 23:07:18 +0200

    Implémentation InterpreteurParcoursTondeusesImpl

--- src/main/java/fr/mowitnow/exercice/metier/impl/InterpreteurParcoursTondeusesImpl.java
+++ src/main/java/fr/mowitnow/exercice/metier/impl/InterpreteurParcoursTondeusesImpl.java
@@ -1,8 +1,12 @@
 package fr.mowitnow.exercice.metier.impl;
 
+import java.util.Arrays;
+import java.util.List;
+
 import fr.mowitnow.exercice.bo.Grille;
 import fr.mowitnow.exercice.bo.InformationsEntree;
 import fr.mowitnow.exercice.bo.Tondeuse;
+import fr.mowitnow.exercice.enumeration.PointCardinal;
 import fr.mowitnow.exercice.metier.InterpreteurParcoursTondeuses;
 
 /**
@@ -11,12 +15,81 @@ import fr.mowitnow.exercice.metier.InterpreteurParcoursTondeuses;
 public class InterpreteurParcoursTondeusesImpl implements InterpreteurParcoursTondeuses {
 
 	/**
+	 * Action rotation droite
+	 */
+	private static final char ACTION_ROTATION_DROITE = 'D';
+	
+	/**
+	 * Action rotation gauche
+	 */
+	private static final char ACTION_ROTATION_GAUCHE = 'G';
+	
+	/**
+	 * Action avancer
+	 */
+	private static final char ACTION_AVANCER = 'A';
+	
+	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public void interpreterParcoursTondeuses(InformationsEntree informationsEntree) {
-		// TODO Auto-generated method stub
+		final Grille grille = informationsEntree.getGrille();
+		final List<Tondeuse> listeTondeuses = informationsEntree.getListeTondeuses();
+
+		listeTondeuses.stream().forEach(tondeuse -> {
+			String instruction = tondeuse.getInstruction();
+			instruction.chars().forEach(action -> {
+				switch (action) {
+					case ACTION_ROTATION_DROITE: {
+							PointCardinal[] pointsCardinaux = PointCardinal.values();
+							int iOrientation = Arrays.binarySearch(pointsCardinaux, tondeuse.getOrientation());
+							iOrientation++;
+							if (iOrientation >= pointsCardinaux.length) {
+								iOrientation = 0;
+							}
+							tondeuse.setOrientation(pointsCardinaux[iOrientation]);
+						}
+						break;
+					case ACTION_ROTATION_GAUCHE: {
+							PointCardinal[] pointsCardinaux = PointCardinal.values();
+							int iOrientation = Arrays.binarySearch(pointsCardinaux, tondeuse.getOrientation());
+							iOrientation--;
+							if (iOrientation < 0) {
+								iOrientation = pointsCardinaux.length - 1;
+							}
+							tondeuse.setOrientation(pointsCardinaux[iOrientation]);
+						}
+						break;
+					case ACTION_AVANCER: 
+						avancerTondeuse(tondeuse);
+						break;
+				}
+			});
+		});
 		
+	}
+	
+	/**
+	 * Avancer une tondeuse
+	 * 
+	 * @param tondeuse
+	 */
+	private void avancerTondeuse(Tondeuse tondeuse) {
+		switch (tondeuse.getOrientation()) {
+			case E:
+				tondeuse.avancerX();
+				break;
+			case N:
+				tondeuse.avancerY();
+				break;
+			case S:
+				tondeuse.reculerY();
+				break;
+			case W:
+				tondeuse.reculerX();
+				break;
+		}
 	}
 
 }
