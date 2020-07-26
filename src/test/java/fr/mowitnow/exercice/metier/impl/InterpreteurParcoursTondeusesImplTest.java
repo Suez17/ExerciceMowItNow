0cf431229f58243dc5e10b3c68c51e1834d1343e
unknown <k-michael@hotmail.fr>
2020-07-26 23:13:33 +0200

    Mise à jour TU

--- src/test/java/fr/mowitnow/exercice/metier/impl/InterpreteurParcoursTondeusesImplTest.java
+++ src/test/java/fr/mowitnow/exercice/metier/impl/InterpreteurParcoursTondeusesImplTest.java
@@ -2,6 +2,9 @@ package fr.mowitnow.exercice.metier.impl;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 import fr.mowitnow.exercice.bo.Grille;
@@ -18,22 +21,40 @@ public class InterpreteurParcoursTondeusesImplTest {
 	public void testInterpreterParcoursTondeuses_ParcoursValide() {
 		InformationsEntree informationsEntree = new InformationsEntree();
 		informationsEntree.setGrille(new Grille(5, 5));
-		Tondeuse tondeuse = new Tondeuse();
-		tondeuse.setOrientation(PointCardinal.N);
-		tondeuse.setPositionX(1);
-		tondeuse.setPositionY(2);
-		tondeuse.setInstruction("GAGAGAGAA");
-		informationsEntree.ajouterTondeuse(tondeuse);
+		Tondeuse tondeuse1 = new Tondeuse();
+		tondeuse1.setOrientation(PointCardinal.N);
+		tondeuse1.setPositionX(1);
+		tondeuse1.setPositionY(2);
+		tondeuse1.setInstruction("GAGAGAGAA");
 		
-		Tondeuse tondeuseExpected = new Tondeuse();
-		tondeuseExpected.setOrientation(PointCardinal.N);
-		tondeuseExpected.setPositionX(1);
-		tondeuseExpected.setPositionY(3);
-		tondeuseExpected.setInstruction("GAGAGAGAA");
+		Tondeuse tondeuse2 = new Tondeuse();
+		tondeuse2.setOrientation(PointCardinal.E);
+		tondeuse2.setPositionX(3);
+		tondeuse2.setPositionY(3);
+		tondeuse2.setInstruction("AADAADADDA");
+		
+		informationsEntree.ajouterTondeuse(tondeuse1);
+		informationsEntree.ajouterTondeuse(tondeuse2);
+		
+		Tondeuse tondeuseExpected1 = new Tondeuse();
+		tondeuseExpected1.setOrientation(PointCardinal.N);
+		tondeuseExpected1.setPositionX(1);
+		tondeuseExpected1.setPositionY(3);
+		tondeuseExpected1.setInstruction("GAGAGAGAA");
+		
+		Tondeuse tondeuseExpected2 = new Tondeuse();
+		tondeuseExpected2.setOrientation(PointCardinal.E);
+		tondeuseExpected2.setPositionX(5);
+		tondeuseExpected2.setPositionY(1);
+		tondeuseExpected2.setInstruction("AADAADADDA");
+		
+		List<Tondeuse> listeTondeusesExpected = new ArrayList<>();
+		listeTondeusesExpected.add(tondeuseExpected1);
+		listeTondeusesExpected.add(tondeuseExpected2);
 		
 		this.interpreteurParcoursTondeuses.interpreterParcoursTondeuses(informationsEntree);
 		
-		assertThat(informationsEntree.getListeTondeuses().get(0)).usingRecursiveComparison().
-			isEqualTo(tondeuseExpected);
+		assertThat(informationsEntree.getListeTondeuses()).usingRecursiveComparison().
+			isEqualTo(listeTondeusesExpected);
 	}
 }
