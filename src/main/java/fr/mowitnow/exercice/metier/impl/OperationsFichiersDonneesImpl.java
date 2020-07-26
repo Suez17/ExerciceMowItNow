bb15d7fbac25cf44fad2bbb6f8ef7c79261e525d
unknown <k-michael@hotmail.fr>
2020-07-26 19:39:40 +0200

    Création de la classe OperationsFichiersDonneesImpl

---
+++ src/main/java/fr/mowitnow/exercice/metier/impl/OperationsFichiersDonneesImpl.java
@@ -0,0 +1,26 @@
+package fr.mowitnow.exercice.metier.impl;
+
+import java.util.List;
+
+import fr.mowitnow.exercice.bo.InformationsEntree;
+import fr.mowitnow.exercice.bo.Tondeuse;
+import fr.mowitnow.exercice.metier.OperationsFichiersDonnees;
+
+/**
+ * Implémentation OperationsFichiersDonnees
+ */
+public class OperationsFichiersDonneesImpl implements OperationsFichiersDonnees {
+
+	@Override
+	public InformationsEntree lireFichierEntree(String chemin) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String creerFichierSortie(List<Tondeuse> listeTondeuses) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
